d524fbe09772eb4b7f8d839f502172758becea0d
demjanok <demjanok@gmail.com>
2025-04-16 07:54:12 -0700

    add admin panel

--- app.py
+++ app.py
@@ -34,11 +34,50 @@ def login():
     return render_template('login.html')
 
 
-#@app.route('/register', methods=['GET', 'POST'])
-#def register():
-#    if request.method == 'POST':
-#        return 'Register successful!'
-#    return render_template('register.html')
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    from models import Video
+
+    videos = Video.query.all()
+    selected_video = None
+    message = ''
+
+    if request.method == 'POST':
+        if 'create' in request.form:
+            # Створити новий запис
+            new_video = Video(
+                title=request.form.get('title'),
+                title_original=request.form.get('title_original'),
+                year=int(request.form.get('year') or 0),
+                genre=request.form.get('genre'),
+                imdb_url=request.form.get('imdb_url'),
+                imdb_rating=request.form.get('imdb_rating'),
+                description=request.form.get('description')
+            )
+            db.session.add(new_video)
+            db.session.commit()
+            message = '✅ Нове відео створено!'
+            videos = Video.query.all()  # оновити список
+        elif 'save' in request.form:
+            # Зберегти існуюче
+            selected_id = request.form.get('video_id')
+            selected_video = Video.query.get(selected_id)
+            if selected_video:
+                selected_video.title = request.form.get('title')
+                selected_video.title_original = request.form.get('title_original')
+                selected_video.year = int(request.form.get('year') or 0)
+                selected_video.genre = request.form.get('genre')
+                selected_video.imdb_url = request.form.get('imdb_url')
+                selected_video.imdb_rating = request.form.get('imdb_rating')
+                selected_video.description = request.form.get('description')
+                db.session.commit()
+                message = '✅ Зміни збережено!'
+        else:
+            selected_id = request.form.get('video_id')
+            selected_video = Video.query.get(selected_id)
+
+    return render_template('admin.html', videos=videos, selected_video=selected_video, message=message)
+
 
 
 @app.route('/video/<string:slug>')
